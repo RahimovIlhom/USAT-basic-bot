7833043b14798c85afcccb6025859af4389dcb8a
ilhomjon <ilhomjonrahimov217@gmail.com>
2024-04-02 02:59:42 +0500

    update static

--- admin/config/urls.py
+++ admin/config/urls.py
@@ -18,9 +18,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from admin.config import settings
+# from admin.config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
